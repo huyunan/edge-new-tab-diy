1c433d592ae5596e545438d1e3cc3227f2986fa8
胡雨南 <18842920976@163.com>
2025-06-27 14:52:40 +0800

    新增个性设置功能

--- background.js
+++ background.js
@@ -6,11 +6,32 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+let newTabUrl = "https://www.baidu.com"
+getNewTabUrl()
+// 获取 newTabUrl = ""
+function getNewTabUrl() {
+  chrome.storage.local.get(['newTabUrl'], (result) => {
+    if (result.newTabUrl) {
+      newTabUrl = result.newTabUrl
+    } else {
+      newTabUrl = "https://www.baidu.com"
+    }
+  })
+}
+// 监听来自 popup 的消息
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "changeUrl") {
+    getNewTabUrl();
+    sendResponse({ msg: 'success' });
+    return true;  // 保持消息通道开放
+  }
+});
+
 function handleCreated(tab) {
   if (tab.pendingUrl == "edge://newtab/" || tab.url == "edge://newtab/"
         || tab.pendingUrl == "chrome://newtab/" || tab.url == "chrome://newtab/") {
       let creating = chrome.tabs.update({
-        url: "https://www.baidu.com",
+        url: newTabUrl,
       });
       creating.then(onCreated, onError);
   }
@@ -18,9 +39,9 @@ function handleCreated(tab) {
 
 chrome.tabs.onCreated.addListener(handleCreated);
 
-chrome.action.onClicked.addListener((tab) => {
-  let creating = chrome.tabs.create({
-    url: "https://www.baidu.com",
-  });
-  creating.then(onCreated, onError);
-});
+// chrome.action.onClicked.addListener((tab) => {
+//   let creating = chrome.tabs.create({
+//     url: newTabUrl,
+//   });
+//   creating.then(onCreated, onError);
+// });
---
+++ popup.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const defaultUrl = 'https://www.baidu.com/'
+  const regexp = new RegExp(
+    '^(https?)://[^\s/$.?#].[^\s]*.*$'
+  )
+
+  // 初始化时获取 newTabUrl
+  getNewTabUrl()
+
+  // 获取 newTabUrl
+  function getNewTabUrl() {
+    chrome.storage.local.get(['newTabUrl'], (result) => {
+      setUrl(result.newTabUrl, 'init')
+    })
+  }
+
+  document.getElementById('save').addEventListener('click', () => {
+    const value = document.getElementById('urlInput').value
+    if (!regexp.test(value)) {
+      setMessage('hint', '输入的链接地址不正确！', 'msg-error')
+      setTimeout(() => {
+        getNewTabUrl()
+      }, 1000)
+    } else {
+      setUrl(value, 'save')
+    }
+  })
+
+  document.getElementById('reset').addEventListener('click', () => {
+    setUrl(defaultUrl, 'reset')
+  })
+
+  function setUrl(url, type) {
+    if (!url) {
+      setMessage('link', null, 'msg-info', defaultUrl)
+      chrome.storage.local.set({ newTabUrl: defaultUrl })
+    }
+    if (type == 'init') {
+      setMessage('link', null, 'msg-info', url)
+    } else if (type == 'save') {
+      chrome.storage.local.set({ newTabUrl: url }, () => {
+        setMessage('hint', '链接地址保存成功！', 'msg-success')
+        setTimeout(() => {
+          setMessage('link', null, 'msg-info', url)
+        }, 1000)
+        chrome.runtime.sendMessage({action: "changeUrl"}, (response) => {
+            console.log(response)
+        });
+      })
+    } else if (type == 'reset') {
+      chrome.storage.local.set({ newTabUrl: url }, () => {
+        setMessage('hint', '重置成功！', 'msg-success')
+        setTimeout(() => {
+          setMessage('link', null, 'msg-info', url)
+        }, 1000)
+        chrome.runtime.sendMessage({action: "changeUrl"}, (response) => {
+            console.log(response)
+        });
+      })
+    }
+  }
+
+  function setMessage(type, msg, cls, url) {
+    if (type == 'hint') {
+      document.getElementById(
+        'content'
+      ).innerHTML = `<div class="${cls} message">${msg}</div>`
+    } else if (type == 'link') {
+      document.getElementById(
+        'content'
+      ).innerHTML = `<div class="${cls} message" title="${url}"><a target="_blank" href="${url}">${url}</a></div>`
+    }
+  }
+})
