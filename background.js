a3a081a221988f0a5cf9dccefe597dc5df37b93e
胡雨南 <18842920976@163.com>
2025-07-09 09:49:03 +0800

    添加右键菜单功能

--- background.js
+++ background.js
@@ -39,9 +39,59 @@ function handleCreated(tab) {
 
 chrome.tabs.onCreated.addListener(handleCreated);
 
-// chrome.action.onClicked.addListener((tab) => {
-//   let creating = chrome.tabs.create({
-//     url: newTabUrl,
-//   });
-//   creating.then(onCreated, onError);
-// });
+chrome.storage.local.get(['switchInput'], (result) => {
+  switchInput = !!result.switchInput
+  setPopup(switchInput)
+})
+
+chrome.action.onClicked.addListener(() => {
+  if (!switchInput) return
+  let creating = chrome.tabs.create({
+    url: newTabUrl,
+  });
+  creating.then(onCreated, onError);
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (changes['switchInput']) {
+    switchInput = changes['switchInput'].newValue
+    setPopup(switchInput)
+  }
+});
+
+chrome.contextMenus.onClicked.addListener(
+  (info) => {
+    if (info.menuItemId != '弹出框') return
+    switchInput = info.checked
+    chrome.storage.local.set({ switchInput })
+  }
+)
+
+// popup 以及右键菜单设置
+async function setPopup(switchInput) {
+  if (switchInput) {
+    chrome.action.setPopup({popup: ''});
+  } else {
+    chrome.action.setPopup({popup: 'popup.html'});
+  }
+  try {
+    await chrome.contextMenus.update('弹出框', {
+      title: `弹出框隐藏`,
+      type: 'checkbox',
+      contexts: ['action'],
+      checked: switchInput
+    });
+  } catch (error) {
+    if (error.toString().indexOf('弹出框') > -1) {
+      chrome.contextMenus.create({
+        id: '弹出框',
+        title: `弹出框隐藏`,
+        type: 'checkbox',
+        checked: switchInput,
+        contexts: ['action']
+      });
+    } else {
+      console.log('error', error)
+    }
+  }
+}
